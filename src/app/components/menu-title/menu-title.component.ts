3136d49e24ac7a1f0d6e1aa0c53f019203f8c853
KesseT <64742407+KetCode@users.noreply.github.com>
2024-01-07 19:12:06 -0300

    changed menu bar, small card size and big card display

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
@@ -9,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class MenuTitleComponent implements OnInit {
+
+  @Input()
+  cardTitle:string= ""
+  @Input()
+  cardDescription:string =""
+  @Input()
+  Id:string="0"
 
   constructor() { }
 
