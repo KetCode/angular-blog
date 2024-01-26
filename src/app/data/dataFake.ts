4257d399b50ef7b77682cd3303978b47a88b97bf
KesseT <64742407+KetCode@users.noreply.github.com>
2024-01-26 10:53:54 -0300

    change dataFake

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,30 @@
 export const dataFake = [
   {
+    "id":"0",
+    "title": "Chat gpt sempre diz a verdade?",
+    "author": "Anonymous",
+    "description": "Estudos afirmam que o chat gpt não está 100% correto, muitas das suas respostas não condizem com a verdade. Ainda devemos continuar usando a plataforma",
+    "photoCover":""
+  },
+  {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "The Evolution of the Technological World: A Double-Edged Sword",
+    "author": "DiscoverTec",
+    "description": "The exponential growth of technology has revolutionized the way we live, work, and interact. From the humble beginnings of the Internet to the rise of artificial intelligence and the Internet of Things, technology has infiltrated every aspect of our lives. But as we continue to advance, the question remains: will technology dominate the world, or can we harness its power to create a better future?",
+    "photoCover":"https://img.freepik.com/fotos-premium/conceito-de-tecnologia-de-globalizacao-com-robo-de-renderizacao-3d-com-conexao-global_493806-2305.jpg?size=626&ext=jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
+    "title": "Big Data e Análise",
+    "author": "Anonymous",
+    "description": "O Big Data e a análise estão mudando a forma como as empresas coletam, armazenam e analisam informações. Isso permite uma tomada de decisão mais rápida e precisa, mas também traz questões éticas sobre a forma como os dados são usados.",
+    "photoCover":"https://img.freepik.com/fotos-premium/tecnologia-de-hospedagem-de-ilustracao-de-computacao-em-nuvem-3d-com-dispositivos-eletronicos_843415-6153.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Realidade Virtual e Aumentada",
+    "author": "Veronica Sherer",
+    "description": "A realidade virtual (VR) e a realidade aumentada (AR) estão mudando a forma como nos conectamos e interagimos com o mundo digital. Embora essas tecnologias sejam empolgantes, também trazem desafios, como a dificuldade de manter a distinção entre a realidade e a ilusão.",
+    "photoCover":"https://geekflare.com/wp-content/uploads/2022/08/Augmented-Reality-AR-vs.-Virtual-Reality-VR-The-Differences.jpg"
+  },
 ]
